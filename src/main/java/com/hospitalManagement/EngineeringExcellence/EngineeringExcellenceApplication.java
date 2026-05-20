38fa9cb02a95a6a9357a80064747e2ac90ef7843
farhaddubey <farhaddubey003@gmail.com>
2026-05-20 11:30:14 +0530

    hospital management

---
+++ src/main/java/com/hospitalManagement/EngineeringExcellence/EngineeringExcellenceApplication.java
@@ -0,0 +1,13 @@
+package com.hospitalManagement.EngineeringExcellence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EngineeringExcellenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EngineeringExcellenceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hospitalManagement/EngineeringExcellence/EngineeringExcellenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hospitalManagement.EngineeringExcellence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EngineeringExcellenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
